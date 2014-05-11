550ddb9730d316fccfbdfa2d405d955c9f62b6c7
parzgnat <tparzgnat@tsgrp.com>
2014-05-10 20:46:35 -0500

    Initial code commit.  Includes sample content model and Share config.

---
+++ AMP/alfrescoAmp/setenv-fletcher2.bat
@@ -0,0 +1,5 @@
+SET JAVA_HOME=C:\Java\jdk1.6.0_35
+SET ANT_HOME=C:\Apache\apache-ant-1.8.4
+SET PATH=%JAVA_HOME%\bin;%ANT_HOME%\bin
+
+SET ALFRESCO_SDK=C:\Users\tparzgnat\Documents\Alfresco\SDK\alfresco-enterprise-sdk-4.1.1.3
